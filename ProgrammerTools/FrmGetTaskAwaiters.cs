7a937fab31d52acb8ab6b252d39c0a576832681c
Mohamed Fayez <mfezooo@gmail.com>
2024-03-29 11:37:16 +0200

    done

--- ProgrammerTools/FrmGetTaskAwaiters.cs
+++ ProgrammerTools/FrmGetTaskAwaiters.cs
@@ -1,14 +1,10 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 
 using System.Windows.Forms;
@@ -32,47 +28,69 @@ namespace ProgrammerTools
         private string getTaskWithOutAwaiter()
         {
             string result = null;
-            var projectPath = @"F:\Test file\Controlllers\Setup";
+            var projectPath = @"F:\Test file\Controlllers\Setup"; 
             var project = new DirectoryInfo(projectPath);
-            var syntaxTrees = project.GetFiles("*Controller.cs", SearchOption.AllDirectories)
+            var syntaxTrees = project.GetFiles("*.cs", SearchOption.AllDirectories)
                 .Select(file => CSharpSyntaxTree.ParseText(File.ReadAllText(file.FullName)));
+
 
             var methodsWithoutAwait = new List<(MethodDeclarationSyntax Method, string FileName, int LineNumber)>();
 
             foreach (var syntaxTree in syntaxTrees)
             {
+
+                string controllerName= getFileName(syntaxTree.ToString());
                 var root = syntaxTree.GetRoot();
                 var methods = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
 
                 foreach (var method in methods)
                 {
-                    if (!ContainsAwait(method))
+                    if (IsAsyncTaskMethod(method) && !ReturnsActionResult(method))
                     {
-                        methodsWithoutAwait.Add((method, syntaxTree.FilePath, method.GetLocation().GetMappedLineSpan().StartLinePosition.Line));
+                        var methodCalls = root.DescendantNodes().OfType<InvocationExpressionSyntax>()
+                            .Where(invocation => invocation.Expression is IdentifierNameSyntax identifier &&
+                                                  identifier.Identifier.Text == method.Identifier.Text)
+                            .ToList();
+
+                        foreach (var methodCall in methodCalls)
+                        {
+                            if (!IsCalledWithAwait(methodCall))
+                            {
+                                methodsWithoutAwait.Add((method, controllerName, methodCall.GetLocation().GetMappedLineSpan().StartLinePosition.Line));
+                            }
+                        }
                     }
                 }
             }
 
             foreach (var (method, fileName, lineNumber) in methodsWithoutAwait)
-            {
-                Console.WriteLine($"Method without await: {method.Identifier} - File: {fileName} - Line: {lineNumber}");
-                result+=$"Method without await: {method.Identifier} - File: {fileName} - Line: {lineNumber} \n";
+            {  
+                result += $"Method without await: Controller: {fileName} - Line: {lineNumber} - Methoud: {method.Identifier} \n";
             }
             return result;
         }
-     
-        private static bool ContainsAwait(MethodDeclarationSyntax method)
-        {
-            // Check if the method is async and returns Task
-            if (method.Modifiers.Any(SyntaxKind.AsyncKeyword) &&
-                method.ReturnType is GenericNameSyntax returnType &&
-                returnType.Identifier.Text == "Task")
-            {
-                // Check if the method contains an await expression
-                return method.DescendantNodes().OfType<AwaitExpressionSyntax>().Any();
-            }
 
-            return false;
+        private static bool IsAsyncTaskMethod(MethodDeclarationSyntax method)
+        {
+            return method.Modifiers.Any(SyntaxKind.AsyncKeyword) &&
+         method.ReturnType is GenericNameSyntax returnType &&
+         returnType.Identifier.Text == "Task";
+        }
+
+        private static bool ReturnsActionResult(MethodDeclarationSyntax method)
+        {
+            return method.ReturnType.ToString() == "ActionResult";
+        }
+
+        private static bool IsCalledWithAwait(InvocationExpressionSyntax invocation)
+        {
+            return invocation.Ancestors().OfType<AwaitExpressionSyntax>().Any();
+        }
+        private string getFileName(string controllerData)
+        {             
+            string[] words = controllerData.Split(' ');
+            string controllerName = words.FirstOrDefault(word => word.EndsWith("Controller"));
+            return controllerName;
         }
     }
 }
