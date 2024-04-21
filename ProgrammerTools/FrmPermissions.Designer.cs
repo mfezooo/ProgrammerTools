57151d04117dfc89cdefa0addf28946ff1e8db32
MFayez <mfayez@IT-DEV>
2024-04-21 13:08:52 +0200

    z

---
+++ ProgrammerTools/FrmPermissions.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProgrammerTools
+{
+    partial class FrmPermissions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbSource = new System.Windows.Forms.RichTextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.rtbstatment = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbSource
+            // 
+            this.rtbSource.Location = new System.Drawing.Point(502, 45);
+            this.rtbSource.Name = "rtbSource";
+            this.rtbSource.Size = new System.Drawing.Size(286, 393);
+            this.rtbSource.TabIndex = 0;
+            this.rtbSource.Text = "rtbSource";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 383);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(456, 46);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // rtbstatment
+            // 
+            this.rtbstatment.Location = new System.Drawing.Point(12, 45);
+            this.rtbstatment.Name = "rtbstatment";
+            this.rtbstatment.Size = new System.Drawing.Size(456, 309);
+            this.rtbstatment.TabIndex = 2;
+            this.rtbstatment.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(502, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SoruceList";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Statment";
+            // 
+            // FrmPermissions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbstatment);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.rtbSource);
+            this.Name = "FrmPermissions";
+            this.Text = "FrmPermissions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbSource;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox rtbstatment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ProgrammerTools/FrmPermissions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammerTools
+{
+    public partial class FrmPermissions : Form
+    {
+        public FrmPermissions()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string textlist = rtbSource.Text;
+            var SelectedList = textlist.Trim().Split(',');
+           var  SelectedPath =@"D:\NewGerated";
+            CreateCustomFileGroup(SelectedList.ToList(), SelectedPath);
+        }
+        private void CreateCustomFileGroup(List<string> sFilesNames, string path)
+        {
+            path += "\\" + "PermissionFiles";
+            string fileNameWithExt = "Permissions.txt";
+            if (!System.IO.Directory.Exists(path))
+            { System.IO.Directory.CreateDirectory(path); }
+            string result = "";
+            foreach (var modelName in sFilesNames)
+            {
+                result += CreateCustomFile(modelName);
+            }
+            using (FileStream fs = File.Create(path + "\\" + fileNameWithExt))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(result);
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+        }
+        private string CreateCustomFile(string ModelName)
+        {
+            ModelName = ModelName.Replace("\n", "");
+            string content  = rtbstatment.Text +"\n \n"; 
+             content  = content.Replace("{0}", ModelName);
+            return content;
+
+
+
+        }
+    }
+}
