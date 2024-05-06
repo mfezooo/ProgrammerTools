2b814620c6fec69e5d890488d75224f53f143389
Mohamed Fayez <mfezooo@gmail.com>
2024-05-06 13:27:16 +0300

    cb

--- ProgrammerTools/HandleLogExceptions.cs
+++ ProgrammerTools/HandleLogExceptions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,48 @@ namespace ProgrammerTools
         private void btnGo_Click(object sender, EventArgs e)
         {
             string filePath = tbPath.Text;
+            slicedLog(filePath);
 
+        }
+        public void slicedLog(string inputFilePath)
+        { 
+            // Specify the path for the output text file
+            string outputFilePath = @"c:\outLog2.txt";
+
+            // Check if the input file exists
+            if (File.Exists(inputFilePath))
+            {
+                // Read the entire input file as one string
+                string fileContent = File.ReadAllText(inputFilePath);
+
+                // Split the file content by the delimiter
+                string[] sections = fileContent.Split(new string[] { "**********************************" }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Create a StreamWriter to write to the output file
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string spcefiedContent = "";
+                    // Iterate through the sections and write the filtered ones to the output file
+                    foreach (string section in sections)
+                    {
+                        spcefiedContent += section;
+                        if (!section.Contains("WebAPI"))
+                        {
+                            writer.WriteLine("Section:");
+                            spcefiedContent += section;
+                            writer.WriteLine(section);
+                            writer.WriteLine("End of Section\n");
+                        }
+                    }
+                    writer.Write(spcefiedContent);
+                }
+
+                Console.WriteLine("Filtered sections have been written to the output file.");
+            }
+            else
+            {
+                Console.WriteLine("Input file not found!");
+            }
         }
     }
 }
--- ProgrammerTools/Program.cs
+++ ProgrammerTools/Program.cs
@@ -16,7 +16,7 @@ namespace ProgrammerTools
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmGetTaskAwaiters());
+            Application.Run(new HandleLogExceptions());
         }
     }
 }
